8fa2411c8992006bb5bbe61d5575260ff5623744
Julien Chakra <julien.chakra@futurice.com>
2017-11-13 18:00:40 +0200

    Create TimelineDriver

---
+++ timeline/drivers/TimelineDriver.ts
@@ -0,0 +1,77 @@
+// import { select } from 'd3-selection'
+import { Stream } from 'xstream'
+import fromEvent from 'xstream/extra/fromEvent'
+
+interface ITimelineSettings {
+  start: {},
+  end: {},
+  cursorColor?: string
+}
+
+export type TimelineSource = {
+  events: (name: string) => Stream<Event>,
+}
+
+export default function makeTimelineDriver(selector: string, settings: ITimelineSettings) {
+  const timelineElt = document.querySelector(selector) as HTMLElement
+
+  function createTimeline() {
+    if (!timelineElt) {
+      return
+    }
+
+    timelineElt.style.position = 'relative'
+    timelineElt.style.cursor = 'crosshair'
+
+    const timelineCursorLine = document.createElement('div')
+    timelineCursorLine.setAttribute('id', 'timeline__cursor--line')
+    timelineCursorLine.style.display = 'none'
+    timelineCursorLine.style.position = 'absolute'
+    timelineCursorLine.style.width = '2px'
+    timelineCursorLine.style.height = '100%'
+    timelineCursorLine.style.backgroundColor = settings.cursorColor || '#555'
+    timelineCursorLine.style.pointerEvents = 'none'
+
+    timelineElt.appendChild(timelineCursorLine)
+  }
+
+  function handleClick() {
+    console.log('click')
+  }
+
+  function handleOver(data) {
+    if (!timelineElt) {
+      return
+    }
+
+    const cursorLine = timelineElt.querySelector('#timeline__cursor--line') as HTMLElement
+    if (cursorLine) {
+      cursorLine.style.display = 'block'
+      cursorLine.style.left = `${data.payload.x}px`
+    }
+  }
+
+  return function timelineDriver(input$) {
+    input$.take(1).addListener({
+      complete: () => { return },
+      error: (e) => { throw e },
+      next: createTimeline,
+    })
+
+    input$.filter((event) => event.type === 'timelineClick').addListener({
+      complete: () => { throw new Error('Timeline sink completed') },
+      error: (e) => { throw e },
+      next: handleClick,
+    })
+
+    input$.filter((event) => event.type === 'timelineOver').addListener({
+      complete: () => { throw new Error('Timeline sink completed') },
+      error: (e) => { throw e },
+      next: handleOver,
+    })
+
+    return {
+      events: (evName) => fromEvent(timelineElt as EventTarget, evName),
+    }
+  }
+}
--- timeline/index.ts
+++ timeline/index.ts
@@ -4,14 +4,17 @@ import onionify, { Reducer, StateSource } from 'cycle-onionify'
 import { Stream } from 'xstream'
 
 import main from 'timeline/App'
+import makeTimelineDriver, { TimelineSource } from './drivers/TimelineDriver'
 
 export interface ISources<S> {
   DOM: DOMSource
+  Timeline: TimelineSource
   onion: StateSource<S>
 }
 
 export interface ISinks<S> {
   DOM: Stream<VNode>
+  Timeline: {}
   onion: Stream<Reducer<S>>
 }
 
@@ -19,4 +22,5 @@ const wrappedMain = onionify(main)
 
 run(wrappedMain, {
   DOM: makeDOMDriver('#main-container'),
+  Timeline: makeTimelineDriver('#timeline', { start: '', end: '' }),
 })
