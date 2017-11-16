93bb331bffbef883c0ed1ab5262e7224038c94ff
Julien Chakra <julien.chakra@futurice.com>
2017-11-16 23:53:08 +0200

    Some header design

--- timeline/Header/index.ts
+++ timeline/Header/index.ts
@@ -1,9 +1,10 @@
-import { div } from '@cycle/dom'
+import { div, i } from '@cycle/dom'
 import xs from 'xstream'
 
 export default function Header() {
   return {
     DOM: xs.of(div('.Header', [
+      i('.material-icons', 'timeline'),
       div('.Header-title', 'Timeline'),
     ])),
   }
